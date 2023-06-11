943c529bc82d924ebf9d0fd70de6ab0c355b54d8
Nguyễn Phi Hùng <82272158+hungneyugn@users.noreply.github.com>
2023-06-11 23:17:51 +0700

    Update Dio.h

--- Dio.h
+++ Dio.h
@@ -1,204 +1,204 @@
 #ifndef __DIO_H
 #define __DIO_H
 #include "stm32f10x.h"
-#define identifyPin				\
+#define identifyPin		\
 	GPIO_TypeDef * gpioPort;\
-	uint16_t gpioPin;				\
+	uint16_t gpioPin;	\
 	switch(ChannelId){\
 		case DIO_CHANNEL_PA0:	\
-			gpioPort = GPIOA;		\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_0;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA1:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_1;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA2:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_2;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA3:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_3;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA4:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_4;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA5:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_5;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA6:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_6;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA7:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_7;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA8:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_8;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PA9:		\
-			gpioPort = GPIOA;			\
+			gpioPort = GPIOA;	\
 			gpioPin = GPIO_Pin_9;	\
-			break;								\
-		case DIO_CHANNEL_PA10:	\
-			gpioPort = GPIOA;			\
-			gpioPin = GPIO_Pin_10;\
-			break;								\
-		case DIO_CHANNEL_PA11:	\
-			gpioPort = GPIOA;			\
-			gpioPin = GPIO_Pin_11;\
-			break;								\
-		case DIO_CHANNEL_PA12:	\
-			gpioPort = GPIOA;			\
-			gpioPin = GPIO_Pin_12;\
-			break;								\
-		case DIO_CHANNEL_PA13:	\
-			gpioPort = GPIOA;			\
-			gpioPin = GPIO_Pin_13;\
-			break;								\
-		case DIO_CHANNEL_PA14:	\
-			gpioPort = GPIOA;			\
-			gpioPin = GPIO_Pin_14;\
-			break;								\
-		case DIO_CHANNEL_PA15:	\
-			gpioPort = GPIOA;			\
-			gpioPin = GPIO_Pin_15;\
-			break;								\
-														\
+			break;			\
+		case DIO_CHANNEL_PA10:		\
+			gpioPort = GPIOA;	\
+			gpioPin = GPIO_Pin_10;	\
+			break;			\
+		case DIO_CHANNEL_PA11:		\
+			gpioPort = GPIOA;	\
+			gpioPin = GPIO_Pin_11;	\
+			break;			\
+		case DIO_CHANNEL_PA12:		\
+			gpioPort = GPIOA;	\
+			gpioPin = GPIO_Pin_12;	\
+			break;			\
+		case DIO_CHANNEL_PA13:		\
+			gpioPort = GPIOA;	\
+			gpioPin = GPIO_Pin_13;	\
+			break;			\
+		case DIO_CHANNEL_PA14:		\
+			gpioPort = GPIOA;	\
+			gpioPin = GPIO_Pin_14;	\
+			break;			\
+		case DIO_CHANNEL_PA15:		\
+			gpioPort = GPIOA;	\
+			gpioPin = GPIO_Pin_15;	\
+			break;			\
+						\
 		case DIO_CHANNEL_PB0:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_0;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB1:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_1;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB2:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_2;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB3:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_3;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB4:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_4;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB5:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_5;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB6:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_6;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB7:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_7;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB8:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_8;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PB9:		\
-			gpioPort = GPIOB;			\
+			gpioPort = GPIOB;	\
 			gpioPin = GPIO_Pin_9;	\
-			break;								\
-		case DIO_CHANNEL_PB10:	\
-			gpioPort = GPIOB;			\
-			gpioPin = GPIO_Pin_10;\
-			break;								\
-		case DIO_CHANNEL_PB11:	\
-			gpioPort = GPIOB;			\
-			gpioPin = GPIO_Pin_11;\
-			break;								\
-		case DIO_CHANNEL_PB12:	\
-			gpioPort = GPIOB;			\
-			gpioPin = GPIO_Pin_12;\
-			break;								\
-		case DIO_CHANNEL_PB13:	\
-			gpioPort = GPIOB;			\
-			gpioPin = GPIO_Pin_13;\
-			break;								\
-		case DIO_CHANNEL_PB14:	\
-			gpioPort = GPIOB;			\
-			gpioPin = GPIO_Pin_14;\
-			break;								\
-		case DIO_CHANNEL_PB15:	\
-			gpioPort = GPIOB;			\
-			gpioPin = GPIO_Pin_15;\
-			break;								\
-														\
+			break;			\
+		case DIO_CHANNEL_PB10:		\
+			gpioPort = GPIOB;	\
+			gpioPin = GPIO_Pin_10;	\
+			break;			\
+		case DIO_CHANNEL_PB11:		\
+			gpioPort = GPIOB;	\
+			gpioPin = GPIO_Pin_11;	\
+			break;			\
+		case DIO_CHANNEL_PB12:		\
+			gpioPort = GPIOB;	\
+			gpioPin = GPIO_Pin_12;	\
+			break;			\
+		case DIO_CHANNEL_PB13:		\
+			gpioPort = GPIOB;	\
+			gpioPin = GPIO_Pin_13;	\
+			break;			\
+		case DIO_CHANNEL_PB14:		\
+			gpioPort = GPIOB;	\
+			gpioPin = GPIO_Pin_14;	\
+			break;			\
+		case DIO_CHANNEL_PB15:		\
+			gpioPort = GPIOB;	\
+			gpioPin = GPIO_Pin_15;	\
+			break;			\
+						\
 		case DIO_CHANNEL_PC0:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_0;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC1:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_1;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC2:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_2;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC3:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_3;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC4:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_4;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC5:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_5;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC6:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_6;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC7:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_7;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC8:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_8;	\
-			break;								\
+			break;			\
 		case DIO_CHANNEL_PC9:		\
-			gpioPort = GPIOC;			\
+			gpioPort = GPIOC;	\
 			gpioPin = GPIO_Pin_9;	\
-			break;								\
-		case DIO_CHANNEL_PC10:	\
-			gpioPort = GPIOC;			\
-			gpioPin = GPIO_Pin_10;\
-			break;								\
-		case DIO_CHANNEL_PC11:	\
-			gpioPort = GPIOC;			\
-			gpioPin = GPIO_Pin_11;\
-			break;								\
-		case DIO_CHANNEL_PC12:	\
-			gpioPort = GPIOC;			\
-			gpioPin = GPIO_Pin_12;\
-			break;								\
-		case DIO_CHANNEL_PC13:	\
-			gpioPort = GPIOC;			\
-			gpioPin = GPIO_Pin_13;\
-			break;								\
-		case DIO_CHANNEL_PC14:	\
-			gpioPort = GPIOC;			\
-			gpioPin = GPIO_Pin_14;\
-			break;								\
-		case DIO_CHANNEL_PC15:	\
-			gpioPort = GPIOC;			\
-			gpioPin = GPIO_Pin_15;\
-			break;\
+			break;			\
+		case DIO_CHANNEL_PC10:		\
+			gpioPort = GPIOC;	\
+			gpioPin = GPIO_Pin_10;	\
+			break;			\
+		case DIO_CHANNEL_PC11:		\
+			gpioPort = GPIOC;	\
+			gpioPin = GPIO_Pin_11;	\
+			break;			\
+		case DIO_CHANNEL_PC12:		\
+			gpioPort = GPIOC;	\
+			gpioPin = GPIO_Pin_12;	\
+			break;			\
+		case DIO_CHANNEL_PC13:		\
+			gpioPort = GPIOC;	\
+			gpioPin = GPIO_Pin_13;	\
+			break;			\
+		case DIO_CHANNEL_PC14:		\
+			gpioPort = GPIOC;	\
+			gpioPin = GPIO_Pin_14;	\
+			break;			\
+		case DIO_CHANNEL_PC15:		\
+			gpioPort = GPIOC;	\
+			gpioPin = GPIO_Pin_15;	\
+			break;			\
 	}	
 #define Dio_PortLevelType uint16_t
 typedef enum{
